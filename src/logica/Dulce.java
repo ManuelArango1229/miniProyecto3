a29ff5eedae9462a8908193f9f1816da0bc2547a
Juan Manuel Arango <juan12292004@outlook.com>
2023-05-09 18:59:04 -0500

    primer commmit, se separo en carpetas principal,logica, interfaz grafica, creacion de clase dulce, implementacion gitignore

---
+++ src/logica/Dulce.java
@@ -0,0 +1,41 @@
+package logica;
+
+public class Dulce{
+    public Dulce(char Tipo,String name){
+        dulce=false;
+        acido=false;
+        sinAzucar=false;
+        this.name=name;
+        switch (Tipo) {
+            case 'D':
+                this.dulce=true;
+                break;
+            case 'A':
+                this.acido=true;
+            case 'S':
+                this.sinAzucar=true;
+        }
+    }
+
+    public char getTipo(){
+        if(dulce){
+            return 'D';
+        }else if(acido){
+            return 'A';
+        }else if(sinAzucar){
+            return 'S';
+        }else{
+            return 'X';
+        }
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    private String name;
+    private boolean dulce;
+    private boolean acido;
+    private boolean sinAzucar;
+}
